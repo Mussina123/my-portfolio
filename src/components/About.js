88a8e7c011e2ccf6c3733fd6df1706f391b2b277
Tyler Beck <softwaretpd2035@gmail.com>
2022-12-01 18:02:19 -0500

    changed bio info

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
 import React from "react";
 import TB from '../images/TB-headshot.jpg'
 
-export default function About(){
-    return(
-     <div
-        name="about"
-        className="w-full h-100 "
-        >
+export default function About() {
+  return (
+    <div
+      name="about"
+      className="w-full h-100 "
+    >
       <div className="sm:pr-2 max-w-screen-lg pt-2 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-black">
-            
+
           </p>
         </div>
 
@@ -18,12 +18,12 @@ export default function About(){
         <h1 className="text-6xl font-bold text-center mt-5">Tyler Beck </h1>
         <h3 className="text-3xl font-medium text-center mt-3"> Full Stack Software Engineer</h3>
         <p className="text-xl mt-5 px-6">
-        I'm a full-stack developer who is highly flexible, an established learner, and continuously expanding my knowledge to maintain current and upcoming coding best practices to maximize performance and quality. I am looking to bring my skills to a tech company with global reach.
+          I'm a full-stack developer who is an established learner, analytical, organized, and a team player who has years of professional collaboration experience. I am looking to bring my skills to a tech company with global reach.
         </p>
       </div>
 
       <div>
-      
+
       </div>
     </div>
   );
